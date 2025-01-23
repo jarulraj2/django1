a5986733d2c68f5390950c1f4da7855db2ce0948
Arulraj <jarulraj2@gmail.com>
2025-01-23 18:23:57 +0530

    added

--- privilege/views.py
+++ privilege/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-  
+def home(request):
+    return render(request, "index.html")
+
+def dashboard(request):
+    name = request.POST['name'].capitalize();
+    return render(request, "dashboard.html", {'name':name})
